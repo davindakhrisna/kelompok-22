3a2b255d38ce5ccf06a9af2f8b1cf30007e71ecb
andrianhidayat1106 <andrianhidayat1106@gmail.com>
2026-06-06 01:15:23 +0700

    feat: add dummy data akun and create login->view,services dan juga route filter role

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // TYPE
 type Akun struct{
 	id int
@@ -57,6 +59,75 @@ type Jadwal struct{
 }
 
 
+var akun = make([]Akun, 0)
+var session Akun
+// var dosen = make([]Dosen,0)
+	// var mahasiswa = make([]Mahasiswa,0)
+	// var kelas = make([]Kelas,0)
+	// var mataKuliah = make([]MataKuliah,0)
+	// var ruangan = make([]Ruangan,0)
+	// var jadwal = make([]Jadwal,0)
 func main(){
 
+	
+
+	// dummy data admin
+	akun = append(akun,Akun{id: 1,email: "admin@telkomuniversity.ac.id",password: "admin@123",role: "admin"})
+	akun = append(akun,Akun{id: 2,email: "andrianhidayat@student.telkomuniversity.ac.id",password: "andre@1",role: "mahasiswa"})
+	akun = append(akun,Akun{id: 2,email: "dimas@telkomuniversity.ac.id",password: "dimas@1",role: "dosen"})
+
+	loginView()
+	routeHalaman(session.role)
+
 }
+func headerApp(){
+	fmt.Println("==============")
+	fmt.Println("   JADWALKU   ")
+	fmt.Println("==============")
+}
+
+func loginView(){
+	headerApp()
+	fmt.Println("Halaman Login")
+	fmt.Println("--------------")
+	fmt.Println()
+	var inputEmail,password string
+	fmt.Print("Email : ")
+	fmt.Scan(&inputEmail)
+	fmt.Print("Password : ")
+	fmt.Scan(&password)
+	loginService(inputEmail,password)
+}
+
+func loginService(email,password string){
+
+	for _,value := range akun{
+		
+		if(value.email == email && value.password == password){
+			session = value
+			return
+		}
+	}
+	fmt.Println()
+	fmt.Println("..............")
+	fmt.Println("Email atau Password anda salah!!")
+	fmt.Println("..............")
+	fmt.Println()
+	loginView()
+}
+
+func routeHalaman(role string){
+	if role == "admin"{
+		fmt.Println("Halaman Admin")
+	}
+
+	if role == "mahasiswa"{
+	fmt.Println("Halaman Mahasiswa")
+	}
+
+	if role == "dosen"{
+	fmt.Println("Halaman Dosen")
+
+	}
+}
+
